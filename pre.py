253c416fff61992ea3edfdb17187ae5f535b08ae
zhurunqiu <zhurunqiu1@163.com>
2024-12-08 16:03:36 +0800

    Update pre.py

--- pre.py
+++ pre.py
@@ -98,7 +98,7 @@ if st.button("Predict"):
 
     if predicted_class == 1:
         advice = (
-            f"Dear Doctor/Patient, according to our model, you have a high risk of occult lymph node metastasis. "
+            f"According to our model, you have a high risk of occult lymph node metastasis. "
             f"The model predicts that your probability of having occult lymph node metastasis is {probability:.1f}%. "
             "While this is just an estimate, it suggests that you may be at significant risk. "
             "I recommend that you receive neck dissection, this procedure is intended to remove any potentially affected lymph nodes in the neck region, "
@@ -106,10 +106,10 @@ if st.button("Predict"):
         )
     else:
         advice = (
-            f"Dear Doctor/Patient, according to our model, you have a low risk of occult lymph node metastasis. "
+            f"According to our model, you have a low risk of occult lymph node metastasis. "
             f"The model predicts that your probability of not having occult lymph node metastasis is {probability:.1f}%. "
-            "However, maintaining a healthy lifestyle is still very important. "
-            "I recommend regular check-ups to monitor your heart health, "
+            "However, maintain a healthy lifestyle is still very important. "
+            "I recommend regular check-ups to monitor your health, "
             "and to seek medical advice promptly if you experience any symptoms."
         )
 
